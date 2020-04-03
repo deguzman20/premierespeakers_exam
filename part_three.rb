97c4d9ba7ea77edd2e456e8dbf353de48fc97bf2
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-04-03 23:25:39 +0800

    Change indention

--- part_three.rb
+++ part_three.rb
@@ -3,10 +3,10 @@ def comparison
   @summary_compare = Order.active.approved.totals_summary(@start_time - 1.year, @end_time - 1.year, "BB")
 
   if @summary_compare.total.to_f < @summary.total.to_f
-      @change = @summary.total.to_f - @summary_compare.total.to_f
-      @summary_pct_change = "#{((@change / (@summary_compare.total.to_f.zero? ? 1 : @summary_compare.total).to_f) * 100).round}% Higher"
+    @change = @summary.total.to_f - @summary_compare.total.to_f
+    @summary_pct_change = "#{((@change / (@summary_compare.total.to_f.zero? ? 1 : @summary_compare.total).to_f) * 100).round}% Higher"
   else
-      @change = @summary_compare.total.to_f - @summary.total.to_f
-      @summary_pct_change = "#{((@change / (@summary_compare.total.to_f.zero? ? 1 : @summary_compare.total).to_f) * 100).round}% Lower"
+    @change = @summary_compare.total.to_f - @summary.total.to_f
+    @summary_pct_change = "#{((@change / (@summary_compare.total.to_f.zero? ? 1 : @summary_compare.total).to_f) * 100).round}% Lower"
   end
 end
